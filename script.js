dd2ab8d76ff08b719cc7ee83951a67c5a49c8759
Kaptain-T <ezighi.antony@gmail.com>
2022-07-09 14:22:41 +0100

    added cart popups

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 const cartImage = document.querySelector('.cartimage');
 const cart = document.querySelector(".cart-container");
+const minus = document.querySelector(".minus");
+const plus = document.querySelector(".plus");
+const quantityNumber = document.querySelector((".quantityNumber"))
+const addToCart = document.querySelector(".addToCart")
+const cartNotif = document.querySelector(".cart-notif")
+const emptyCart = document.querySelector(".emptycart-container")
+let x = 0;
 
+// ************* ADD TO CART ***************
+minus.addEventListener("click", function(){
+    if (x>0) {
+        --x;
+        quantityNumber.innerHTML = x;
+        return x
+        
+    } else {
+        x=0
+        return x
+    }
+    
+})
 
+plus.addEventListener("click", function () {
+    ++x;
+    quantityNumber.innerHTML = x
+    return x
+});
 
-
+addToCart.addEventListener("click", function(){
+    if (quantityNumber.innerHTML > 0 ) {
+        cartNotif.innerHTML = quantityNumber.innerHTML
+        cartNotif.classList.add("display");
+    } else {
+       cartNotif.classList.remove("display");
+     }
+})
 
 
 
@@ -13,11 +45,19 @@ const cart = document.querySelector(".cart-container");
 
 
 cartImage.addEventListener('click', function(){
-    // if (cart.style.display == 'none') {
-    //     cart.style.display = 'block'
-        
-    // } else {
-    //     cart.style.display = 'none';
-    // }
-    cart.style.display = "none";
+    if (cartNotif.innerHTML > 0) {
+        if (cart.classList.contains("display")) {
+          cart.classList.remove("display");
+        } else {
+          cart.classList.add("display");
+        }
+    } else {
+        if (emptyCart.classList.contains("display")) {
+          emptyCart.classList.remove("display");
+        } else {
+          emptyCart.classList.add("display");
+        }
+    }
+    
+    
 });
