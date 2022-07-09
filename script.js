d4075c1396decad48ae98c34993b9371d1510904
Kaptain-T <ezighi.antony@gmail.com>
2022-07-09 03:55:49 +0100

    first commit - nothing special yet

---
+++ script.js
@@ -0,0 +1,23 @@
+const cartImage = document.querySelector('.cartimage');
+const cart = document.querySelector(".cart-container");
+
+
+
+
+
+
+
+
+
+
+
+
+cartImage.addEventListener('click', function(){
+    // if (cart.style.display == 'none') {
+    //     cart.style.display = 'block'
+        
+    // } else {
+    //     cart.style.display = 'none';
+    // }
+    cart.style.display = "none";
+});
